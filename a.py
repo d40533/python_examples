9611c3c43c58b0cca27626f5c6dac408f3173339
emilie.liu <d40533@yahoo.com.tw>
2023-02-26 17:30:35 +0800

    initial commit

---
+++ a.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Create a 2-D array, set every second element in
+# some rows and find max per row:
+
+x = np.arange(15, dtype=np.int64).reshape(3, 5)
+x[1:, ::2] = -99
+print("ok")
---
+++ b.py
@@ -0,0 +1,7 @@
+#import pandas 
+import numpy as np
+import pandas as pd
+#a = pandas.__version__
+# a=  pd.Series([1, 3, 5, np.nan, 6, 8])
+a = pd.date_range("20130101", periods=6)
+print(a)
